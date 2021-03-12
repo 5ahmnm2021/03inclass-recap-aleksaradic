24970ac1392582b3bcfc198004dc018b2e55f5c7
aleksaradic <aleksaradic01@gmail.com>
2021-03-12 13:20:19 +0100

    Try/Catch Aufgabe erledigt

--- Assets/Scripts/addNumbers.cs
+++ Assets/Scripts/addNumbers.cs
@@ -8,11 +8,30 @@ public class addNumbers : MonoBehaviour
     public InputField num1_if;
     public InputField num2_if;
     public Text result;
+    
+    float num1;
+    float num2;
 
     public void AddNumbersOnClick()
     {
-        float num1 = float.Parse(num1_if.text);
-        float num2 = float.Parse(num2_if.text);
+        try
+        {
+            float num1 = float.Parse(num1_if.text);
+        }
+        catch (System.Exception)
+        {
+            num1_if.image.color = new Color32(255,0,0,100);
+        }
+
+        try
+        {
+            float num2 = float.Parse(num2_if.text);
+        }
+        catch (System.Exception)
+        {
+            num2_if.image.color = new Color32(255,0,0,100);
+        }
+        
         float addResult = num1 + num2;
         result.text = addResult.ToString();
     }
