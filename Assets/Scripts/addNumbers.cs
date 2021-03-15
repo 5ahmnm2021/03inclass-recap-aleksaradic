f4f9e4967826b719ef050c27abaa463591ea6c23
aleksaradic <aleksaradic01@gmail.com>
2021-03-15 12:07:38 +0100

    Es soll funktionieren, aber es funktioniert nicht

--- Assets/Scripts/addNumbers.cs
+++ Assets/Scripts/addNumbers.cs
@@ -12,27 +12,47 @@ public class addNumbers : MonoBehaviour
     float num1;
     float num2;
 
+    bool bool_if1;
+    bool bool_if2;
+
     public void AddNumbersOnClick()
     {
         try
         {
             float num1 = float.Parse(num1_if.text);
+            bool bool_if1 = true;
+            //Debug.Log("Try1 geht");
         }
         catch (System.Exception)
         {
             num1_if.image.color = new Color32(255,0,0,100);
+            bool bool_if1 = false;
+            //Debug.Log("Catch1 geht");
         }
 
         try
         {
             float num2 = float.Parse(num2_if.text);
+            bool bool_if2 = true;
+            //Debug.Log("Try2 geht");
         }
         catch (System.Exception)
         {
             num2_if.image.color = new Color32(255,0,0,100);
+            bool bool_if2 = false;
+            //Debug.Log("Catch1 geht");
         }
         
-        float addResult = num1 + num2;
-        result.text = addResult.ToString();
+        if (bool_if1 == true | bool_if2 == true)
+        {
+            float addResult = num1 + num2;
+            result.text = addResult.ToString();
+            //Debug.Log("Beide Stimmen");
+        }
+        else
+        {
+            result.text = "Ohje";
+            //Debug.Log("Ohje kommt");
+        }
     }
 }
